2b1d7f460df9ba2a4c5897084571cbcb86cb1b00
zhanjun <zhanjun@artela.network>
2024-04-17 15:04:52 +0800

    route

---
+++ src/app/api/zealy/clam/route.ts
@@ -0,0 +1,27 @@
+import {type  NextRequest, NextResponse} from 'next/server'
+
+
+export async function GET(request: NextRequest) {
+
+    console.log(request.nextUrl.searchParams);
+    return NextResponse.json({ret: 'GET Success'}, {status: 200});
+}
+
+export async function POST(request: NextRequest) {
+    console.log(request);
+    const DefaultKey = '4313993YLGHtMqnYPyovXz512lm'; // Replace with your API key configured in Zealy task
+
+    const requestHeaders = new Headers(request.headers)
+    const apiKey = requestHeaders.get("x-api-key")
+
+    if (apiKey !==DefaultKey) {
+        return NextResponse.json({ret: 'Invalid API Key'}, {status: 400});
+    }
+
+
+    const res = await request.json()
+    const success = true; // Replace with custom logic
+
+
+    return NextResponse.json(res, {status: 200});
+}
