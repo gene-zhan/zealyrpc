678f0b8257cbd682c0e722a6449277ad8905f55e
zhanjun <zhanjun@artela.network>
2024-04-19 17:56:35 +0800

    add  seed

--- src/app/api/zealy/clam/route.ts
+++ src/app/api/zealy/clam/route.ts
@@ -1,8 +1,8 @@
 import {type  NextRequest, NextResponse} from 'next/server'
 import {db} from '../../../../../dizzle.config'
-import {zealyQuizTask,flexiQuizTask} from "../../../../../db/drizzle/schema";
+import {zealyQuizTask, flexiQuizTask} from "../../../../../db/drizzle/schema";
 import {varchar} from "drizzle-orm/pg-core";
-import { sql ,eq} from 'drizzle-orm';
+import {sql, eq, count, and} from 'drizzle-orm';
 
 
 export async function GET(request: NextRequest) {
@@ -41,20 +41,20 @@ export async function POST(request: NextRequest) {
     //     twitterUname: res?.accounts?.twitterUname,
     // });
 
-    if(res?.accounts?.email == null) {
+    if (res?.accounts?.email == null) {
         return NextResponse.json({ret: 'accounts email is null'}, {status: 400});
     }
-    const result = await db.select().from(flexiQuizTask).where(eq(flexiQuizTask.emailAddress, res?.accounts?.email))
-    console.log("zealyresult: " + JSON.stringify(result));
 
-    if(result[0].percentageScore){
-       const     percentageScore = parseInt(result[0].percentageScore, 10);
-       if(percentageScore>=60){
-           return NextResponse.json({ret: 'Passed'}, {status: 200});
-       }else {
-           return NextResponse.json({ret: 'Failed,This score is ' + percentageScore + ' which is too low.'}, {status: 400});
-       }
+    const queryResult = await db.select({count: count()}).from(flexiQuizTask).where(and(
+        eq(flexiQuizTask.emailAddress, res?.accounts?.email),
+        eq(flexiQuizTask.pass, 'true')
+    ));
+    console.log("zealyresult: " ,queryResult);
+
+
+    if (queryResult[0].count == 1) {
+        return NextResponse.json({ret: 'Passed'}, {status: 200});
+    } else {
+        return NextResponse.json({ret: 'Failed,This score not passed'}, {status: 400});
     }
-
-    return NextResponse.json({ret:'can not find percentage Score with email'}, {status: 400});
 }
