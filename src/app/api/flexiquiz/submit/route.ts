2b232b622fee538015050201e0b8a3e09f809052
zhanjun <zhanjun@artela.network>
2024-04-19 14:55:45 +0800

    add  seed

--- src/app/api/flexiquiz/submit/route.ts
+++ src/app/api/flexiquiz/submit/route.ts
@@ -1,7 +1,9 @@
 import {type  NextRequest, NextResponse} from 'next/server'
+import jsonpath from 'jsonpath';
 
 import {db} from '../../../../../dizzle.config'
 import {flexiQuizTask} from "../../../../../db/drizzle/schema";
+import {eq} from 'drizzle-orm';
 export async function GET(request: NextRequest) {
 
     console.log(request.nextUrl.searchParams);
@@ -13,13 +15,22 @@ export async function POST(request: NextRequest) {
     const res = await request.json()
     console.log("submit: " + JSON.stringify(res));
 
+    const email: string | undefined = jsonpath.query(res, '$.data.registration_fields[?(@.name=="Email address")].value')[0];
+    const lastName: string | undefined = jsonpath.query(res, '$.data.registration_fields[?(@.name=="Last name")].value')[0];
+    const firstName: string | undefined = jsonpath.query(res, '$.data.registration_fields[?(@.name=="First name")].value')[0];
+
+
+    if (email === undefined) return NextResponse.json({ret: 'Invalid email'}, {status: 200}); // No email (email)
+
+    await db.delete(flexiQuizTask).where(eq(flexiQuizTask.emailAddress, email));
+
     await db.insert(flexiQuizTask).values({
         eventId: res?.event_id,
         responseId: res?.data?.response_id,
         questId: res?.data?.quiz_id,
-        firstName: res?.data?.first_name,
-        lastName: res?.data?.last_name,
-        emailAddress: res?.data?.email_address,
+        firstName: firstName,
+        lastName: lastName,
+        emailAddress: email,
         userId: res?.data?.user_id,
         points: res?.data?.points,
         availablePoints: res?.data?.available_points,
