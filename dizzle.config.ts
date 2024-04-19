86f532e4b6a6b7f121eb628e6cf2724be164f9dc
zhanjun <zhanjun@artela.network>
2024-04-19 11:29:53 +0800

    add  seed

--- dizzle.config.ts
+++ dizzle.config.ts
@@ -1,17 +1,17 @@
-import {Config} from "drizzle-kit";
+import {defineConfig} from "drizzle-kit";
 import * as dotenv from "dotenv";
-import { sql } from "@vercel/postgres";
-import { drizzle } from 'drizzle-orm/vercel-postgres';
+import {sql} from "@vercel/postgres";
+import {drizzle} from 'drizzle-orm/vercel-postgres';
 
 dotenv.config({path: ".env"});
-export default {
+export default defineConfig({
     schema: "./db/schema/*",
     out: "./db/drizzle",
     driver: 'pg',
     dbCredentials: {
         connectionString: process.env.POSTGRES_URL,
     }
-} satisfies Config;
+});
 export const db = drizzle(sql);
 
 // drizzle-kit introspect:pg --config=dizzle.config.ts
